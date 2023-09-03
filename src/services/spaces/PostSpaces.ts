e624ed1c36cf04fcc93512dbfc0141d79a4c976c
aleks.stserbatski <aleks.stserbatski@moneysupermarket.com>
2023-09-03 23:17:17 +0100

    marshalling

--- src/services/spaces/PostSpaces.ts
+++ src/services/spaces/PostSpaces.ts
@@ -1,23 +1,22 @@
 import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { v4 } from "uuid";
 
 export async function postSpaces(event: APIGatewayProxyEvent, ddbClient: DynamoDBClient): Promise<APIGatewayProxyResult> {
 
     const randomId = v4();
-    const item = JSON.parse(event.body);
+
+    const item = {
+        id: randomId,
+        location: JSON.parse(event.body).location
+    }
 
     const result = await ddbClient.send(new PutItemCommand({
         TableName: process.env.TABLE_NAME,
-        Item: {
-            id: {
-                S: randomId
-            },
-            location: {
-                S: item.location
-            }
-        }
+        Item: marshall(item)
     }));
+    
     console.log(result);
 
     return {
